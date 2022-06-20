3d167b623d98609a2c52efedab6d9838f5f04d4e
LeVi4869 <thanhvinhle3900@gmail.com>
2022-06-20 18:39:33 +0700

    styling the login page a bit

--- Django201/profiles/urls.py
+++ Django201/profiles/urls.py
@@ -6,5 +6,5 @@ app_name = "profiles"
 
 urlpatterns = [
     path("<str:username>/", views.ProfileDetailView.as_view(), name="detail"),
-    path("<str:username>/follow", views.FollowView.as_view(), name="follow"),
+    path("<str:username>/follow/", views.FollowView.as_view(), name="follow"),
 ]

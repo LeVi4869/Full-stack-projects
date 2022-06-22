5252b2ac246ae879cf2b4ffcce16302ca3534df0
LeVi4869 <thanhvinhle3900@gmail.com>
2022-06-22 16:18:29 +0700

    adding some models and templates for wagtail project

--- sevendays/generic/models.py
+++ sevendays/generic/models.py
@@ -4,8 +4,10 @@ from wagtail.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 
 
-class HomePage(Page):
-    banner_title = models.CharField(max_length=100, default="Welcome to my homepage")
+class GenericPage(Page):
+    banner_title = models.CharField(max_length=100, default="Welcome to my generic page!")
+    introduction = models.TextField(blank=True)
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
+        FieldPanel("introduction"),
     ]
